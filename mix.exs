e62b4665129273ccc11c21bb4dc5b889f79ad43c
Justin Schneck <jschneck@mac.com>
2023-04-11 13:59:31 -0400

    v2.2.0 release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Peridiod.MixProject do
   def project do
     [
       app: :peridiod,
-      version: "2.1.0",
+      version: "2.2.0",
       elixir: "~> 1.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
