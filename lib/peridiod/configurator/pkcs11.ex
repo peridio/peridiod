4267bba2f29a63d4400b88769b080225560ebe51
Justin Schneck <j.schneck@peridio.com>
2024-05-15 17:12:49 -0400

    Add openssl3 pkcs11 engine search paths

--- lib/peridiod/configurator/pkcs11.ex
+++ lib/peridiod/configurator/pkcs11.ex
@@ -33,7 +33,9 @@ defmodule Peridiod.Configurator.PKCS11 do
   defp pkcs11_path() do
     [
       "/usr/lib/engines-1.1/libpkcs11.so",
-      "/usr/lib/engines/libpkcs11.so"
+      "/usr/lib/engines/libpkcs11.so",
+      "/usr/lib/engines-3/libpkcs11.so",
+      "/usr/lib64/engines-3/libpkcs11.so"
     ]
     |> Enum.find(&File.exists?/1)
   end
