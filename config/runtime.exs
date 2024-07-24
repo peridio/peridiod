ea5e30b9dcb29338524ca75e40fdf33dfcf9bee9
Justin Schneck <j.schneck@peridio.com>
2024-07-24 14:29:36 -0400

    Allow the log level to be set from env PERIDIO_LOG_LEVEL

--- config/runtime.exs
+++ config/runtime.exs
@@ -4,3 +4,13 @@ shell =
   System.find_executable("bash") || System.find_executable("zsh") || System.find_execurtable("sh")
 
 System.put_env("SHELL", shell)
+
+log_level =
+  case System.get_env("PERIDIO_LOG_LEVEL") do
+    "debug" -> :debug
+    "warning" -> :warning
+    "info" -> :info
+    _ -> :error
+  end
+
+config :logger, level: log_level
