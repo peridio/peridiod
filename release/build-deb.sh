b525d6673afa7b1a909f041ddab08c0a7db592a1
Justin Schneck <j.schneck@peridio.com>
2024-08-01 10:49:10 -0400

    build rpm packages

--- release/build-deb.sh
+++ release/build-deb.sh
@@ -1,28 +1,29 @@
 #!/usr/bin/env sh
 
+set -e
+
 # Args
-# 1) PERIDIOD_RELEASE_ARCHIVE
-# 2) PERIDIOD_DEB_PACKAGE_DIR
-# 
+# 1) PERIDIOD_RELEASE_ARCHIVE: path to the release archive
+# 2) PERIDIOD_PACKAGE_DIR: path to the package build dir
 
 PERIDIOD_RELEASE_ARCHIVE=$1
-PERIDIOD_DEB_PACKAGE_DIR=$2
+PERIDIOD_PACKAGE_DIR=$2
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 
-mkdir -p "$PERIDIOD_DEB_PACKAGE_DIR/DEBIAN" \
-  "$PERIDIOD_DEB_PACKAGE_DIR/opt/peridiod" \
-  "$PERIDIOD_DEB_PACKAGE_DIR/etc/peridiod" \
-  "$PERIDIOD_DEB_PACKAGE_DIR/usr/lib/systemd/system/" \
-  "$PERIDIOD_DEB_PACKAGE_DIR/usr/lib/systemd/system.conf.d"
+mkdir -p "$PERIDIOD_PACKAGE_DIR/DEBIAN" \
+  "$PERIDIOD_PACKAGE_DIR/usr/lib/peridiod" \
+  "$PERIDIOD_PACKAGE_DIR/etc/peridiod" \
+  "$PERIDIOD_PACKAGE_DIR/usr/lib/systemd/system/"
 
-envsubst < "$SCRIPT_DIR/deb/control" > "$PERIDIOD_DEB_PACKAGE_DIR/DEBIAN/control"
-envsubst < "$SCRIPT_DIR/deb/changelog" > "$PERIDIOD_DEB_PACKAGE_DIR/DEBIAN/changelog"
-cp release/deb/postinstall "$PERIDIOD_DEB_PACKAGE_DIR/DEBIAN"
-cp release/deb/prerm "$PERIDIOD_DEB_PACKAGE_DIR/DEBIAN"
+envsubst < "$SCRIPT_DIR/deb/control" > "$PERIDIOD_PACKAGE_DIR/DEBIAN/control"
+envsubst < "$SCRIPT_DIR/deb/changelog" > "$PERIDIOD_PACKAGE_DIR/DEBIAN/changelog"
+cp "$SCRIPT_DIR/deb/postinstall" "$PERIDIOD_PACKAGE_DIR/DEBIAN"
+cp "$SCRIPT_DIR/deb/prerm" "$PERIDIOD_PACKAGE_DIR/DEBIAN"
 
-tar -xvf "$PERIDIOD_RELEASE_ARCHIVE" -C "$PERIDIOD_DEB_PACKAGE_DIR/opt/peridiod"
-cp release/peridiod.service "$PERIDIOD_DEB_PACKAGE_DIR/usr/lib/systemd/system/"
-cp release/peridiod.conf "$PERIDIOD_DEB_PACKAGE_DIR/usr/lib/systemd/system.conf.d/"
+tar -xvf "$PERIDIOD_RELEASE_ARCHIVE" -C "$PERIDIOD_PACKAGE_DIR/usr/lib/peridiod"
+cp "$SCRIPT_DIR/peridiod.service" "$PERIDIOD_PACKAGE_DIR/usr/lib/systemd/system/"
+cp "$SCRIPT_DIR/peridiod.env" "$PERIDIOD_PACKAGE_DIR/etc/peridiod/"
+cp "$SCRIPT_DIR/pkg-peridio.json" "$PERIDIOD_PACKAGE_DIR/etc/peridiod/peridio.json"
 
-dpkg-deb --build "$PERIDIOD_DEB_PACKAGE_DIR"
+dpkg-deb --build "$PERIDIOD_PACKAGE_DIR"
