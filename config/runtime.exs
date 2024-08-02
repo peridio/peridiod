7a2ce33589f9c5019441f352fe3d0899610d3038
Justin Schneck <j.schneck@peridio.com>
2024-08-02 16:02:59 -0400

    fixup runtime config

--- config/runtime.exs
+++ config/runtime.exs
@@ -5,29 +5,26 @@ shell =
 
 System.put_env("SHELL", shell)
 
-log_level =
-  case System.get_env("PERIDIO_LOG_LEVEL") do
-    "debug" -> :debug
-    "warning" -> :warning
-    "info" -> :info
-    _ -> :error
-  end
+case System.get_env("PERIDIO_LOG_LEVEL") do
+  nil -> :noop
+  "debug" -> config :logger, level: :debug
+  "warning" -> config :logger, level: :warning
+  "info" -> config :logger, level: :info
+  _ -> config :logger, level: :error
+end
 
-kv_backend =
-  case System.get_env("PERIDIO_KV_BACKEND") do
-    "filesystem" ->
-      path = System.get_env("PERIDIO_KV_BACKEND_FILESYSTEM_PATH", "/var/peridiod")
-      file = System.get_env("PERIDIO_KV_BACKEND_FILESYSTEM_FILE", "peridiod-state")
-      {PeridiodPersistence.KVBackend.Filesystem, path: path, file: file}
+case System.get_env("PERIDIO_KV_BACKEND") do
+  "filesystem" ->
+    path = System.get_env("PERIDIO_KV_BACKEND_FILESYSTEM_PATH", "/var/peridiod")
+    file = System.get_env("PERIDIO_KV_BACKEND_FILESYSTEM_FILE", "peridiod-state")
 
-    "ubootenv" ->
-      PeridiodPersistence.KVBackend.UBootEnv
+    config :peridiod_persistence,
+      kv_backend: {PeridiodPersistence.KVBackend.Filesystem, path: path, file: file}
 
-    _ ->
-      PeridiodPersistence.KVBackend.UBootEnv
-  end
+  "ubootenv" ->
+    config :peridiod_persistence,
+      kv_backend: PeridiodPersistence.KVBackend.UBootEnv
 
-config :peridiod_persistence,
-  kv_backend: kv_backend
-
-config :logger, level: log_level
+  _ ->
+    :noop
+end
