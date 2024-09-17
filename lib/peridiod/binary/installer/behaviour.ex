5cb389a7d1cd577252626d02a60b0f45b40b8347
Justin Schneck <j.schneck@peridio.com>
2024-09-17 16:06:41 +0200

    Add missing behaviour to Binary Installer

--- lib/peridiod/binary/installer/behaviour.ex
+++ lib/peridiod/binary/installer/behaviour.ex
@@ -12,7 +12,7 @@ defmodule Peridiod.Binary.Installer.Behaviour do
       end
 
       def install_update(%Peridiod.Binary{}, {:error, reason}, state) do
-        {:error, reason}
+        {:error, reason, state}
       end
 
       def install_finish(%Peridiod.Binary{}, :invalid_signature, state) do
@@ -23,7 +23,15 @@ defmodule Peridiod.Binary.Installer.Behaviour do
         {:ok, installer_state}
       end
 
-      defoverridable install_init: 3, install_update: 3, install_finish: 3, install_info: 2
+      def install_error(%Peridiod.Binary{}, error, installer_state) do
+        {:error, error, installer_state}
+      end
+
+      defoverridable install_init: 3,
+                     install_update: 3,
+                     install_finish: 3,
+                     install_info: 2,
+                     install_error: 3
     end
   end
 
@@ -35,6 +43,10 @@ defmodule Peridiod.Binary.Installer.Behaviour do
   @callback install_init(Binary.t(), :cache | :download, installer_opts, installer_state()) ::
               {:ok, installer_state()} | {:error, reason :: any(), installer_state()}
   @callback install_update(Binary.t(), data :: binary(), installer_state()) ::
+              {:ok, installer_state()} | {:error, reason :: any(), installer_state()}
+  @callback install_info(msg :: any, installer_state()) ::
+              {:ok, installer_state()} | {:error, reason :: any(), installer_state()}
+  @callback install_error(Binary.t(), reason :: any(), installer_state()) ::
               {:ok, installer_state()} | {:error, reason :: any(), installer_state()}
   @callback install_finish(Binary.t(), :valid_signature | :invalid_signature, installer_state()) ::
               {:stop, reason :: any(), installer_state()}
